b82b6865e5dc6db016ca70a88d4c9fd275b020b8
HankofallTrades <HankofallTrades@users.noreply.github.com>
2025-04-28 10:43:28 +0200

    more mobile ui tests- prev column wrapping & indicator occlusion

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -17,7 +17,7 @@ const Index = () => {
   };
 
   return (
-    <div className="container mx-auto p-4 max-w-3xl">
+    <div className="container mx-auto p-4">
       <header className="flex flex-col items-center justify-between mb-8 text-center">
         <h1 className="text-3xl md:text-4xl font-bold mb-2 text-fitnessBlue dark:text-fitnessBlue">Stratos</h1>
         <p className="text-gray-600 dark:text-gray-400 mb-6">Take your health & fitness to new heights</p>
